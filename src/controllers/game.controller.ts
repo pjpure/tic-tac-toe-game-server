e3c0298e9622c3f34bc99b6ad1f1078ca1973240
pjpure <pure254326@gmail.com>
2022-03-17 17:44:03 +0700

    add gameUpdate

--- src/controllers/game.controller.ts
+++ src/controllers/game.controller.ts
@@ -3,18 +3,17 @@ import gameService from "../services/game.service";
 
 function GameController(io: Server, socket: Socket) {
 
-    const gameStart = (roomId: string) => {
-        const room = gameService.gameStart(roomId);
+    const gameUpdate = ({ roomId, idx }: { roomId: string, idx: number }) => {
+        console.log(`gameUpdate: ${roomId} ${idx}`);
+        const room = gameService.gameUpdate(roomId, socket.id, idx);
+        if (room == -1) {
+            return;
+        }
         const player1 = room.players[0];
         const player2 = room.players[1];
-        io.to(player1.id).emit("game:start", { player: player1, board: room.board });
-        io.to(player2.id).emit("game:start", { player: player2, board: room.board });
+        io.to(player1.id).emit("game:updated", { player: player1, board: room.board });
+        io.to(player2.id).emit("game:updated", { player: player2, board: room.board });
     }
-
-    const gameUpdate = ({ roomId, data }: { roomId: string, data: any }) => {
-        socket.to(roomId).emit("game:update", data);
-    }
-
     socket.on("game:update", gameUpdate);
 }
 
--- src/services/game.service.ts
+++ src/services/game.service.ts
@@ -15,7 +15,35 @@ const gameStart = (roomId: string) => {
     return rooms[roomId];
 }
 
+const queueUpdate = (room: any) => {
+    const firstPlayer = room.players[0];
+    let newPlayers = room.players.slice(1);
+    newPlayers.push(firstPlayer);
+    room.players = newPlayers;
+    room.players.forEach((player: Player, index: number) => {
+        if (index == 0) {
+            player.isTurn = true;
+        } else {
+            player.isTurn = false;
+        }
+    });
+    return room;
+}
+
+const gameUpdate = (roomId: string, playerId: string, idx: number) => {
+    let room = rooms[roomId];
+    if (playerId !== room.players[0].id) {
+        return -1;
+    }
+    room.board[idx] = room.players[0].symbol;
+    room = queueUpdate(room);
+
+    rooms[roomId] = room;
+    return room;
+}
+
 
 export default {
-    gameStart
+    gameStart,
+    gameUpdate
 }
