32e2b9b89aced01b3902d013f62871d6c6cb2b71
pjpure <pure254326@gmail.com>
2022-03-18 00:37:31 +0700

    add dotenv

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,15 @@
 import app from './app';
 import socketServer from './socket';
 import { createServer } from "http";
+import dotenv from 'dotenv';
+dotenv.config();
 
 const httpServer = createServer(app);
 socketServer(httpServer);
 const port = 8000;
 
 try {
-    httpServer.listen(port, (): void => {
+    httpServer.listen(process.env.PORT || port, (): void => {
         console.log(`Connected successfully on port ${port}`);
     });
 } catch (error: any) {
