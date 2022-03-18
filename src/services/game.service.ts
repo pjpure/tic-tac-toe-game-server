8db1172796b2fa2e6258567cbe6d15b90834becd
pjpure <pure254326@gmail.com>
2022-03-19 00:42:05 +0700

    check win update

--- src/services/game.service.ts
+++ src/services/game.service.ts
@@ -52,8 +52,17 @@ const gameUpdate = (roomId: string, playerId: string, idx: number) => {
             return -1;
         }
         room.board[idx] = room.players[0].symbol;
+        const { isWin, board } = gameCheckWinner(room.board, room.players[0].symbol, idx);
+        if (isWin) {
+            room.status = "ended";
+            room.players.forEach((player: Player) => {
+                if (player.id === playerId) {
+                    player.isWinner = true;
+                }
+            })
+            room.board = board;
+        }
         room = queueUpdate(room);
-
         rooms[roomId] = room;
         return room;
     } catch (e) {
@@ -130,11 +139,89 @@ const gamePlayAgain = (roomId: string, playerId: string) => {
 
 }
 
+const gameCheckWinner = (board: string[], symbol: string, idx: number) => {
+    const boardSize = Math.sqrt(board.length);
+    const row = Math.floor(idx / boardSize);
+    const col = idx % boardSize;
+
+    let rowCount = 0;
+    let colCount = 0;
+    let diag1Count = 0;
+    let diag2Count = 0;
+
+    //row check
+    for (let i = boardSize * row; i < boardSize * (row + 1); i++) {
+        if (board[i] === symbol) {
+            rowCount++;
+        } else {
+            break;
+        }
+    }
+    if (rowCount === boardSize) {
+        for (let i = boardSize * row; i < boardSize * (row + 1); i++) {
+            board[i] = 'W' + symbol;
+        }
+        return { isWin: true, board: board };
+    }
+
+    //column check
+    for (let i = col; i < board.length; i += boardSize) {
+        if (board[i] === symbol) {
+            colCount++;
+        } else {
+            break;
+        }
+    }
+    if (colCount === boardSize) {
+        for (let i = col; i < board.length; i += boardSize) {
+            board[i] = 'W' + symbol;
+        }
+        return { isWin: true, board: board };
+    }
+
+    //diagonal1 check
+    if (row === col) {
+        for (let i = 0; i < board.length; i += boardSize + 1) {
+            if (board[i] === symbol) {
+                diag1Count++;
+            } else {
+                break;
+            }
+        }
+        if (diag1Count === boardSize) {
+            for (let i = 0; i < board.length; i += boardSize + 1) {
+                board[i] = 'W' + symbol;
+            }
+            return { isWin: true, board: board };
+        }
+    }
+
+    //diagonal2 check
+    if (row + col === boardSize - 1) {
+        for (let i = boardSize - 1; i < board.length - (boardSize - 1); i += boardSize - 1) {
+            if (board[i] === symbol) {
+                diag2Count++;
+            } else {
+                break;
+            }
+        }
+        if (diag2Count === boardSize) {
+            for (let i = boardSize - 1; i < board.length - (boardSize - 1); i += boardSize - 1) {
+                board[i] = 'W' + symbol;
+            }
+            return { isWin: true, board: board };
+        }
+
+    }
+    return { isWin: false, board: board };
+}
+
 
 export default {
     gameStart,
     gameUpdate,
     gameWait,
     gameGiveUp,
-    gamePlayAgain
+    gamePlayAgain,
+    gameCheckWinner
 }
